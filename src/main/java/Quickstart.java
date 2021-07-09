632f908b21736f3a34674d932db4b48a1617b562
Ankur <red.rukna@gmail.com>
2021-07-09 14:52:00 -0700

    library update
    java version update
    
    update
    
    adding popular senders
    
    converting to csv writer
    
    updating the file type

--- src/main/java/Quickstart.java
+++ src/main/java/Quickstart.java
@@ -6,60 +6,41 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
-import com.google.api.services.gmail.model.ListMessagesResponse;
-import com.google.api.services.gmail.model.Message;
-import com.google.api.services.gmail.model.MessagePartHeader;
+import com.google.api.services.gmail.model.*;
 import models.HeaderInfo;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
 
 
 //@Slf4j
 public class Quickstart {
     static final Logger log = LogManager.getLogger(Quickstart.class.getName());
+    static final String sendersPath = Paths.get("log", "popularSenders.csv").toString();
     /**
      * Application name.
      */
     private static final String APPLICATION_NAME =
-        "Gmail API Java Quickstart";
+            "Gmail API Java Quickstart";
 
     /**
      * Directory to store user credentials for this application.
      */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
-        System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
-
-    /**
-     * Global instance of the {@link FileDataStoreFactory}.
-     */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
+            System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
     /**
      * Global instance of the JSON factory.
      */
     private static final JsonFactory JSON_FACTORY =
-        JacksonFactory.getDefaultInstance();
-
-    /**
-     * Global instance of the HTTP transport.
-     */
-    private static HttpTransport HTTP_TRANSPORT;
-
+            GsonFactory.getDefaultInstance();
     /**
      * Global instance of the scopes required by this quickstart.
      * <p>
@@ -67,7 +48,15 @@ public class Quickstart {
      * at ~/.credentials/gmail-java-quickstart
      */
     private static final List<String> SCOPES =
-        Arrays.asList(GmailScopes.MAIL_GOOGLE_COM);
+            Arrays.asList(GmailScopes.MAIL_GOOGLE_COM);
+    /**
+     * Global instance of the {@link FileDataStoreFactory}.
+     */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static HttpTransport HTTP_TRANSPORT;
 
     static {
         try {
@@ -87,22 +76,21 @@ public class Quickstart {
      */
     public static Credential authorize() throws IOException {
         // Load client secrets.
-        final InputStream in =
-            Quickstart.class.getResourceAsStream("/client_secret.json");
+        final InputStream in = Quickstart.class.getResourceAsStream("/client_secret.json");
         final GoogleClientSecrets clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
         final GoogleAuthorizationCodeFlow flow =
-            new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
-                .setAccessType("offline")
-                .build();
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
         final Credential credential = new AuthorizationCodeInstalledApp(
-            flow, new LocalServerReceiver()).authorize("user");
+                flow, new LocalServerReceiver()).authorize("user");
         System.out.println(
-            "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
         return credential;
     }
 
@@ -115,22 +103,22 @@ public class Quickstart {
     public static Gmail getGmailService() throws IOException {
         final Credential credential = authorize();
         return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-            .setApplicationName(APPLICATION_NAME)
-            .build();
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 
     public static void main(final String[] args) throws IOException {
-
+        log.info("STARTING----");
         // Populate Bad Senders
         final Set<String> badSenders = getBadSenders();
-
+        final Map<String, Integer> popularSenders = new HashMap<>();
         // Build a new authorized API client service.
         final Gmail service = getGmailService();
 
         // Print the labels in the user's account.
         final String user = "me";
         final ListLabelsResponse listResponse =
-            service.users().labels().list(user).execute();
+                service.users().labels().list(user).execute();
         final List<Label> labels = listResponse.getLabels();
         if (labels.size() == 0) {
             System.out.println("No labels found.");
@@ -143,49 +131,59 @@ public class Quickstart {
 
         ListMessagesResponse listMessagesResponse = service.users().messages().list(user).execute();
         String nextPageToken = listMessagesResponse.getNextPageToken();
+
+        long runningCount = 0;
         while (listMessagesResponse.getMessages().size() > 0) {
             for (final Message message : listMessagesResponse.getMessages()) {
-
-                final Message currentMessage = service.users().messages().get(user, message.getId()).execute();
-                final List<MessagePartHeader> headerList = currentMessage.getPayload().getHeaders();
-                final HeaderInfo headerInfo = new HeaderInfo();
-                headerInfo.setId(currentMessage.getId());
-                for (final MessagePartHeader header : headerList) {
-                    if ("Subject".equals(header.getName())) {
-                        headerInfo.setSubject(header.getValue());
-                    }
-                    if ("From".equals(header.getName())) {
-                        headerInfo.setFrom(header.getValue());
-                    }
-                    if ("Date".equals(header.getName())) {
-                        headerInfo.setDate(header.getValue());
-                    }
-                }
-
-
-
-                boolean deleted = false;
-
-//                Delete Promotions
-                if(currentMessage.getLabelIds().contains("CATEGORY_PROMOTIONS")||currentMessage.getLabelIds().contains("CATEGORY_SOCIAL")){
-                    service.users().messages().delete(user, headerInfo.getId()).execute();
-                    deleted=true;
-                }
-                else {
-//                    Delete bad Senders
-                    for (final String badSender : badSenders) {
-                        if (headerInfo.getFrom() != null && headerInfo.getFrom().contains(badSender)) {
-                            service.users().messages().delete(user, headerInfo.getId()).execute();
-                            deleted = true;
+                try {
+                    runningCount++;
+                    if (runningCount % 500 == 0) {
+                        log.info("event=msgsRead count={}", runningCount);
+                        try (final BufferedWriter bw = new BufferedWriter(new FileWriter(sendersPath))) {
+                            bw.write("email,count\n");
+                            for (final Map.Entry<String, Integer> e : popularSenders.entrySet()) {
+                                bw.write(e.getKey() + "," + e.getValue() + "\n");
+                            }
+                        } catch (final Exception ex) {
+                            log.error("event=errorUpdatingPopularSenders", ex);
                         }
                     }
-                }
-                if (deleted) {
-                    log.info("Deleting Message: {} - {}",currentMessage.getLabelIds(), headerInfo);
-                } else {
-//                log.info("List: {}", headerInfo);
-                }
+                    final Message currentMessage = service.users().messages().get(user, message.getId()).execute();
+                    final List<MessagePartHeader> headerList = currentMessage.getPayload().getHeaders();
+                    final HeaderInfo headerInfo = new HeaderInfo();
+                    headerInfo.setId(currentMessage.getId());
+                    for (final MessagePartHeader header : headerList) {
+                        if ("Subject".equals(header.getName())) {
+                            headerInfo.setSubject(header.getValue());
+                        }
+                        if ("From".equals(header.getName())) {
+                            headerInfo.setFrom(header.getValue());
+                        }
+                        if ("Date".equals(header.getName())) {
+                            headerInfo.setDate(header.getValue());
+                        }
+                    }
+                    final String from = getFrom(headerInfo);
+                    log.info("event=from from={}", from);
 
+                    //update popular senders
+                    popularSenders.put(from, popularSenders.getOrDefault(from, 0) + 1);
+
+//                Delete Promotions
+                    if (currentMessage.getLabelIds().contains("CATEGORY_PROMOTIONS") || currentMessage.getLabelIds().contains("CATEGORY_SOCIAL")) {
+                        service.users().messages().delete(user, headerInfo.getId()).execute();
+                        writeToFileAndLog("bad_category", from, currentMessage, headerInfo);
+
+                    } else {
+//                    Delete bad Senders
+                        if (badSenders.contains(from)) {
+                            service.users().messages().delete(user, headerInfo.getId()).execute();
+                            writeToFileAndLog("bad_sender", from, currentMessage, headerInfo);
+                        }
+                    }
+                } catch (final Exception ex) {
+                    log.error("event=errorInProcess", ex);
+                }
 
             }
             listMessagesResponse = service.users().messages().list(user).setPageToken(nextPageToken).execute();
@@ -194,15 +192,43 @@ public class Quickstart {
 
     }
 
+
+    private static void writeToFileAndLog(final String reason, final String from, final Message currentMessage, final HeaderInfo headerInfo) {
+
+        log.info("event=Deleting reason={} from={} labels={} details={}", reason, from, currentMessage.getLabelIds(), headerInfo);
+        final String filePath = Paths.get("log", "Emails", String.format("%s_%s_%s.txt", reason, from, headerInfo.getDate())).toString();
+        log.info("event=FilePath path={}", filePath);
+        try (final BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write(headerInfo.getSubject());
+            bw.write("\n");
+            if (currentMessage.getPayload() != null && currentMessage.getPayload().getBody() != null && currentMessage.getPayload().getBody().getData() != null) {
+                bw.write(new String(Base64.decodeBase64(currentMessage.getPayload().getBody().getData())));
+            }
+        } catch (final Exception ex) {
+            log.error("event=errorWritingToFile", ex);
+        }
+    }
+
+    private static String getFrom(final HeaderInfo headerInfo) {
+        final String from_complete = headerInfo.getFrom().toLowerCase();
+        final int fromSt = from_complete.lastIndexOf('<');
+        final int fromEn = from_complete.lastIndexOf('>');
+        if (fromSt >= 0) {
+            return from_complete.substring(fromSt + 1, fromEn);
+        } else {
+            return from_complete;
+        }
+    }
+
     public static Set<String> getBadSenders() {
         final Set<String> badSenders = new HashSet<>();
         // Load client secrets.
         final InputStream in = Quickstart.class.getResourceAsStream("/badSenders.txt");
-        try (InputStreamReader isr = new InputStreamReader(in)) {
+        try (final InputStreamReader isr = new InputStreamReader(in)) {
             final BufferedReader br = new BufferedReader(isr);
             String line;
             while ((line = br.readLine()) != null) {
-                badSenders.add(line);
+                badSenders.add(line.toLowerCase());
             }
         } catch (final Exception ex) {
             ex.printStackTrace();
